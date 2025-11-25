18f40c6b6d24f03b5808e669eac69036115909da
kuinleguli <kuinleguli@users.noreply.github.com>
2025-11-25 14:22:21 +0000

    Add endpoint-hooks-account.js, factory-template.js

---
+++ frontend/web/src/endpoint-hooks-account.js
@@ -0,0 +1,5 @@
+import { ethers } from 'ethers';
+
+export const styles_report = () => { return null; };
+
+
---
+++ frontend/web/src/factory-template.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+interface example_layouts_configProps { data: string }
+
+
